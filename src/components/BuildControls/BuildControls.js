31dd06345ea7b1167b3a510cd2131051d4e22a83
rossa <fashionfish@naver.com>
2021-01-23 16:52:42 +0900

    Connecting State to Build Controls

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -7,13 +7,17 @@ const controls = [
   { label: "Salad", type: "salad" },
   { label: "Bacon", type: "bacon" },
   { label: "Cheese", type: "cheese" },
-  { label: "Meat", type: "Meat" },
+  { label: "Meat", type: "meat" },
 ];
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
     {controls.map((ctrl) => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+      />
     ))}
   </div>
 );
